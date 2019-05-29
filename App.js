cd6355b3d8b913476d5ba2abae2b917f26edac3b
Feiyang_Yang <Feiyang_Yang@dell.com>
2019-05-29 22:39:44 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+
+export default class App extends Component {
+  _onPressButton(e) {
+    Alert.alert('You tapped the button!')
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={e => this._onPressButton(e)}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
